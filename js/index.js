b27dd87e5060b26664e956c717426812ca6c8ed8
matieuruth <matieu.ruth@gmail.com>
2020-04-08 20:56:08 -0400

    created append and prepend

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ const siteContent = {
     "contact-h4" : "Contact",
     "address" : "123 Way 456 Street Somewhere, USA",
     "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "e-mail" : "sales@greatidea.io",
   },
   "footer": {
     "copyright" : "Copyright Great Idea! 2018"
@@ -55,10 +55,15 @@ navItems[4].innerText = "About"
 navItems[5].setAttribute('class', '.nav-item-4')
 navItems[5].innerText = "Contact"
 
+navItems.forEach((item) => {
+  item.style.color = 'green';
+})
+
 // for(i=0; i < navItems.length; i++){
 //   siteContent['nav'].innerText = navItems[i]
 // }
 
+//header
 let header = document.querySelector('h1');
 header.textContent = siteContent['cta']['h1'];
 
@@ -71,16 +76,46 @@ bodyImg.setAttribute('src', siteContent['main-content']['middle-img-src'])
 let buttonTxt = document.querySelector('button');
 buttonTxt.textContent = siteContent['cta']['button']
 
-let mainContentItems = document.querySelectorAll('h4');
-mainContentItems[0].textContent= siteContent['main-content']['features-h4']
-mainContentItems[1].textContent= siteContent['main-content']['about-h4']
-mainContentItems[2].textContent= siteContent['main-content']['services-h4']
-mainContentItems[3].textContent= siteContent['main-content']['product-h4']
-mainContentItems[4].textContent= siteContent['main-content']['vision-h4']
+//main content
+let mainContentTitles = document.querySelectorAll('h4');
+mainContentTitles[0].textContent = siteContent['main-content']['features-h4']
+mainContentTitles[1].textContent = siteContent['main-content']['about-h4']
+mainContentTitles[2].textContent = siteContent['main-content']['services-h4']
+mainContentTitles[3].textContent = siteContent['main-content']['product-h4']
+mainContentTitles[4].textContent = siteContent['main-content']['vision-h4']
 
 let mainContentPara = document.querySelectorAll('.text-content p');
-mainContentPara[0].textContent= siteContent['main-content']['features-content']
-mainContentPara[1].textContent= siteContent['main-content']['about-content']
-mainContentPara[2].textContent= siteContent['main-content']['services-content']
-mainContentPara[3].textContent= siteContent['main-content']['product-content']
-mainContentPara[4].textContent= siteContent['main-content']['vision-content']
+mainContentPara[0].textContent = siteContent['main-content']['features-content']
+mainContentPara[1].textContent = siteContent['main-content']['about-content']
+mainContentPara[2].textContent = siteContent['main-content']['services-content']
+mainContentPara[3].textContent = siteContent['main-content']['product-content']
+mainContentPara[4].textContent = siteContent['main-content']['vision-content']
+
+//contact
+let contactTitle = document.querySelectorAll('.contact h4');
+contactTitle[0].textContent = siteContent['contact']['contact-h4']
+
+let contactPara = document.querySelectorAll('.contact p');
+contactPara[0].textContent = siteContent['contact']['address']
+contactPara[1].textContent = siteContent['contact']['phone']
+contactPara[2].textContent = siteContent['contact']['e-mail']
+
+//footer
+let footerPara = document.querySelectorAll('footer p');
+footerPara[0].textContent = siteContent['footer']['copyright']
+
+//appendChild
+let newContent = document.createElement("p");
+newContent.textContent = "Site created by Alanna Mathieu";
+newContent.style.textAlign = 'center'
+
+let body = document.querySelector('body');
+body.appendChild(newContent)
+
+//prepend
+let prependContent = document.createElement("p");
+prependContent.textContent = 'Welcome';
+prependContent.style.textAlign = 'center'
+
+let text = document.querySelector('body');
+text.prepend(prependContent)
