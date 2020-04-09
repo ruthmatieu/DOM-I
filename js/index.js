5a0f99a2c4d60aa6f2f375e0049b3e481fd6b89b
matieuruth <matieu.ruth@gmail.com>
2020-04-08 20:02:51 -0400

    completed task 1 & 2

--- js/index.js
+++ js/index.js
@@ -39,4 +39,48 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let navItems = document.querySelectorAll("a");
+navItems[0].setAttribute('class', '.nav-item-1')
+navItems[0].innerText = "Services"
+navItems[1].setAttribute('class', '.nav-item-2')
+navItems[1].innerText = "Product"
+navItems[2].setAttribute('class', '.nav-item-3')
+navItems[2].innerText = "Vision"
+navItems[3].setAttribute('class', '.nav-item-4')
+navItems[3].innerText = "Features"
+navItems[4].setAttribute('class', '.nav-item-4')
+navItems[4].innerText = "About"
+navItems[5].setAttribute('class', '.nav-item-4')
+navItems[5].innerText = "Contact"
+
+// for(i=0; i < navItems.length; i++){
+//   siteContent['nav'].innerText = navItems[i]
+// }
+
+let header = document.querySelector('h1');
+header.textContent = siteContent['cta']['h1'];
+
+let mainImg = document.getElementById('cta-img');
+mainImg.setAttribute('src', siteContent['cta']['img-src'])
+
+let bodyImg = document.getElementById('middle-img');
+bodyImg.setAttribute('src', siteContent['main-content']['middle-img-src'])
+
+let buttonTxt = document.querySelector('button');
+buttonTxt.textContent = siteContent['cta']['button']
+
+let mainContentItems = document.querySelectorAll('h4');
+mainContentItems[0].textContent= siteContent['main-content']['features-h4']
+mainContentItems[1].textContent= siteContent['main-content']['about-h4']
+mainContentItems[2].textContent= siteContent['main-content']['services-h4']
+mainContentItems[3].textContent= siteContent['main-content']['product-h4']
+mainContentItems[4].textContent= siteContent['main-content']['vision-h4']
+
+let mainContentPara = document.querySelectorAll('.text-content p');
+mainContentPara[0].textContent= siteContent['main-content']['features-content']
+mainContentPara[1].textContent= siteContent['main-content']['about-content']
+mainContentPara[2].textContent= siteContent['main-content']['services-content']
+mainContentPara[3].textContent= siteContent['main-content']['product-content']
+mainContentPara[4].textContent= siteContent['main-content']['vision-content']
